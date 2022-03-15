d0b772b6a7e2c75c00e9bb90ea59dd210587a239
Muskan <muskanchhatrasal@gmail.com>
2022-03-15 07:02:49 +0530

    Setup React Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import { Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from '../src/Components/Navbar/navbar'
+import Home from '../src/Components/Home/home'
+import Products from '../src/Components/Products/products'
+import Footer from '../src/Components/Footer/footer'
+import Cart from '../src/Components/Cart/cart'
+import Wishlist from '../src/Components/Wishlist/wishlist'
 
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/wishlist" element={<Wishlist />}/>
+    </Routes>
+    <Footer />
+    </>
   );
 }
 
