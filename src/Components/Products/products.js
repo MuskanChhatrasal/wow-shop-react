1d2e61540134af2eccd5c307a73007d43ab9a73c
Muskan <muskanchhatrasal@gmail.com>
2022-03-15 13:13:46 +0530

    Listed all the products

--- src/Components/Products/products.js
+++ src/Components/Products/products.js
@@ -1,9 +1,106 @@
 import React from 'react'
 import './products.css'
+import { products } from '../../backend/db/products'
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <div>
+      <section className="main-container">
+        <div className="filters">
+            <span className="title">Filter Products</span>
+            <span>
+              <label  className="form-label"><h2>Price: </h2></label>
+              <br />
+
+              {/* <!-- IMPORTED FROM MY COMPONENT LIBRARY  --> */}
+             <input type="range" min="1" max="100" value="50" style={{width: '20rem'}} />
+            </span>
+            <span>
+                <label>
+                  <input type="radio" name="group1" /> <h3>Ascending</h3>
+                </label>
+            </span>
+            <span>
+                <label>
+                   <input type="radio" name="group1" /> 
+                   <h3>Descending</h3>
+                </label>
+            </span>
+            <span>
+                <label>
+                  <input type="checkbox" name="group1" /> <h3>Include Out of Stock</h3>
+                </label>
+            </span>
+            <span>
+                <label>
+                  <input type="checkbox" name="group1" /> <h3>Fast Delivery Only</h3>
+                </label>
+            </span>
+            <span>
+               <label style={{fontSize: '1.8rem'}}>Category: </label>
+                  <select style={{marginLeft: '1rem', width: '10rem'}}>
+                     <option value="Chineese">Chineese</option>
+                     <option value="Indian">Indian</option>
+                     <option value="Italian">Italian</option>
+                     <option value="Indian">Indian</option>
+                     <option value="Italian">Italian</option>
+                  </select>
+            </span>
+            <label>
+                    <h3 className="filter-rating">Rating: </h3>
+                    <div className="filter-stars">
+                        <i className="far fa-star"></i>
+                        <i className="far fa-star"></i>
+                        <i className="far fa-star"></i>
+                        <i className="far fa-star"></i>
+                        <i className="far fa-star"></i>
+                    </div>
+            </label>
+            <button className="btn filter-btn">Clear Filters</button>
+        </div>
+
+        <section className="categories" id="categories">
+    
+        
+            <div className="box-container">
+                
+                {/* <!-- IMPORTED FROM MY COMPONENT LIBRARY --> */}
+                {products.map((item)=>{
+                  return (
+                    <div className="card card-ecom mg-1-all">
+                    <div className="card-img-cont">
+                        <img className="card-img" src={item.imageUrl}
+                            alt="veg-momos" />
+                    </div>
+                    <div className="card-badge">OFFER</div>
+                    <div className="card-body">
+                        <div className="card-header">
+                            <h6 className="card-title">{item.title}</h6>
+                            <p className="card-desc">{item.categoryName}</p>
+                            <div className="card-price">
+                                <span className="price-new" style={{fontSize: '1.25rem'}}>Rs.{item.price}</span>
+                            </div>
+                        </div>
+                        <div className="card-footer">
+                            <button className="button btn-primary card-button">ADD TO CART</button>
+                        </div>
+                    </div>
+                </div>
+                  )
+                })}
+
+        
+               
+
+                
+            </div>
+        
+        </section>
+        
+        {/* <!-- categories section ends --> */}
+
+    </section>
+    </div>
   )
 }
 
--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -8,23 +8,82 @@ import { v4 as uuid } from "uuid";
 export const products = [
   {
     _id: uuid(),
-    title: "You Can WIN",
-    author: "Shiv Khera",
-    price: "5000",
-    categoryName: "non-fiction",
+    title: "Vegetable-Pizza",
+    availableOrNot: "Available",
+    price: "150/-",
+    categoryName: "Italian",
+    imageUrl: 'https://www.sightseeingtoursitaly.com/wp-content/uploads/2019/06/Famous-Italian-dishes.jpg'
   },
   {
     _id: uuid(),
-    title: "You are Winner",
-    author: "Junaid Qureshi",
-    price: "3000",
-    categoryName: "horror",
+    title: "Stuffed-Bread",
+    availableOrNot: "Not-Available",
+    price: "100/-",
+    categoryName: "Italian",
+    imageUrl: 'https://www.cookingvdo.com/wp-content/uploads/2017/05/ita.jpg'
   },
   {
     _id: uuid(),
-    title: "Think and Grow Rich",
-    author: "Shiv Khera",
-    price: "1000",
-    categoryName: "fiction",
+    title: "Momos",
+    availableOrNot: "Available",
+    price: "80/-",
+    categoryName: "Chineese",
+    imageUrl: 'https://purewows3.imgix.net/images/articles/2021_12/traditional-chinese-food-FB.jpg?auto=format,compress&cs=strip'
+  },
+  {
+    _id: uuid(),
+    title: "Noodles",
+    availableOrNot: "Available",
+    price: "200/-",
+    categoryName: "Chineese",
+    imageUrl: 'https://c.ndtvimg.com/2018-09/i9993f38_noodles_625x300_26_September_18.jpg'
+  },
+  {
+    _id: uuid(),
+    title: "Fried-Potato",
+    availableOrNot: "Not-Available",
+    price: "250/-",
+    categoryName: "Spanish",
+    imageUrl: 'https://www.expatica.com/app/uploads/sites/2/2016/09/croquetas.jpg'
+  },
+  {
+    _id: uuid(),
+    title: "Fries",
+    availableOrNot: "Available",
+    price: "60/-",
+    categoryName: "Spanish",
+    imageUrl: 'https://nomadparadise.com/wp-content/uploads/2020/04/spanish-foods-004-1024x640.jpg.webp'
+  },
+  {
+    _id: uuid(),
+    title: "Plain-Thali",
+    availableOrNot: "Available",
+    price: "90/-",
+    categoryName: "North-Indian",
+    imageUrl: 'https://www.srigurunalapaka.in/assets/images/2.jpg'
+  },
+  {
+    _id: uuid(),
+    title: "Shahi-Thali",
+    availableOrNot: "Available",
+    price: "200/-",
+    categoryName: "North-Indian",
+    imageUrl: 'https://imgmedia.lbb.in/media/2020/11/5fa0387bec0d8e30ee455776_1604335739366.jpg'
+  },
+  {
+    _id: uuid(),
+    title: "Masala-Dosa",
+    availableOrNot: "Available",
+    price: "70/-",
+    categoryName: "South-Indian",
+    imageUrl: 'https://d4t7t8y8xqo0t.cloudfront.net/admin/eazymedia/trends/3091/16164196070.jpg'
+  },
+  {
+    _id: uuid(),
+    title: "Special-Thali",
+    availableOrNot: "Available",
+    price: "200/-",
+    categoryName: "South-Indian",
+    imageUrl: 'https://cdn.5280.com/2020/10/Thali-platter_Bawarchi-Biryani-Point-Denver-Facebook.jpg'
   },
 ];
