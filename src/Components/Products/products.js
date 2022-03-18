42df82990e429a0c4df8b6fbdf969dca3207e16d
Muskan <muskanchhatrasal@gmail.com>
2022-03-18 14:49:30 +0530

    Filter functionality: for exclude and offer part

--- src/Components/Products/products.js
+++ src/Components/Products/products.js
@@ -1,38 +1,42 @@
 import React, {useReducer} from 'react'
 import './products.css'
 import { products } from '../../backend/db/products'
+// import Rating from '../Rating/rating';
 
-
-let exludeFlag = true;
-const reducer = (state, action) =>{
+const filterReducer = (state, action) =>{
   switch(action.type){
     case 'EXCLUDE_NOT_AVAILABLE':
-      return {state, filteredProducts: excludeNotAvailable()}
-    case 'INCLUDE_NOT_AVAILABLE':
-      return {state, filteredProducts: includeNotAvailable()}
+      return {...state, excludeNotAvailable: !state.excludeNotAvailable}
+    case 'ONLY_ITEMS_WITH_OFFER':
+      return {...state, offerItems: !state.offerItems}
+
+    case 'CLEAR_ALL_FILTERS': 
+      return {excludeNotAvailable: false, offerItems: false, byRating: 0, byCategory: '', byPrice: 0}
   }
 }
 
 
-const excludeNotAvailable = () =>{
-  exludeFlag = false;
-  let updatedList;
-  updatedList = products.filter((item)=>item.availableOrNot);
-  return updatedList;
-}
-
-const includeNotAvailable = () =>{
-   exludeFlag = true;
-   let updatedList;
-   updatedList = products;
-   return updatedList;
-}
-
 const Products = () => {
+
+ const [filterState, filterDispatch] = useReducer(filterReducer, {excludeNotAvailable: false, offerItems: false, byRating: 0, byCategory: '', byPrice: 0})
+
  
-  const [state, dispatch] = useReducer(reducer, {filteredProducts: []})
- 
+const updatedProducts = () => {
+  let updatedProductList = products;
   
+  if(filterState.excludeNotAvailable){
+    updatedProductList = updatedProductList.filter((item)=> item.availableOrNot)
+    console.log(updatedProductList)
+  }
+
+  if(filterState.offerItems){
+    updatedProductList = updatedProductList.filter((item)=>item.offer)
+    console.log(updatedProductList)
+  }
+
+  return updatedProductList;
+}
+
 
   return (
     <div>
@@ -59,14 +63,13 @@ const Products = () => {
             </span>
             <span>
                 <label>
-                  <input type="checkbox" name="group1" 
-                  onChange={()=>exludeFlag ? dispatch({type: 'EXCLUDE_NOT_AVAILABLE'}):dispatch({type: 'INCLUDE_NOT_AVAILABLE'})}/> 
+                  <input type="checkbox" name="group1"  onChange={()=>filterDispatch({type: 'EXCLUDE_NOT_AVAILABLE'})} checked={filterState.excludeNotAvailable} /> 
                   <h3>Exclude Not Available</h3>
                 </label>
             </span>
             <span>
                 <label>
-                  <input type="checkbox" name="group1" /> <h3>Item With Offers Only</h3>
+                  <input type="checkbox" name="group1" onChange={()=>filterDispatch({type: 'ONLY_ITEMS_WITH_OFFER'})} checked={filterState.offerItems} /> <h3>Item With Offers Only</h3>
                 </label>
             </span>
             <span>
@@ -82,14 +85,15 @@ const Products = () => {
             <label>
                     <h3 className="filter-rating">Rating: </h3>
                     <div className="filter-stars">
+                         <i className="far fa-star"></i>
                         <i className="far fa-star"></i>
                         <i className="far fa-star"></i>
                         <i className="far fa-star"></i>
-                        <i className="far fa-star"></i>
-                        <i className="far fa-star"></i>
+                        <i className="far fa-star"></i> 
+                        
                     </div>
             </label>
-            <button className="btn filter-btn">Clear Filters</button>
+            <button className="btn filter-btn" onClick={()=>filterDispatch({type: 'CLEAR_ALL_FILTERS'})}>Clear Filters</button>
         </div>
 
         <section className="categories" id="categories">
@@ -98,7 +102,7 @@ const Products = () => {
             <div className="box-container">
                 
                 {/* <!-- IMPORTED FROM MY COMPONENT LIBRARY --> */}
-                {exludeFlag ? products.map((item)=>{
+                {updatedProducts().map((item)=>{
                   return (
                     <div className="card card-ecom mg-1-all" style={{marginTop: '1rem'}}>
                     <button class="btn secondary-text-btn-sm card-close"><i class="far fa-heart"></i></button>
@@ -123,37 +127,7 @@ const Products = () => {
                     </div>
                 </div>
                   )
-                }) : state.filteredProducts.map((item)=>{
-                      return (
-                        <div className="card card-ecom mg-1-all" style={{marginTop: '1rem'}}>
-                    <button class="btn secondary-text-btn-sm card-close"><i class="far fa-heart"></i></button>
-                    <div className="card-img-cont">
-                        <img className="card-img" src={item.imageUrl}
-                            alt="veg-momos" />
-                    </div>
-                    {item.offer && <div className="card-badge card-offer">{item.offerOFF} OFF</div>}
-                    <div className="card-body">
-                        <div className="card-header">
-                            <h6 className="card-title">{item.title}</h6>
-                            <p className="card-desc">{item.categoryName}</p>
-                            <div className="card-price">
-                                <span className="price-new" style={{fontSize: '1.25rem'}}>Rs.{item.priceNew}</span>
-                                {item.offer && <span className="price-old" style={{fontSize: '1.25rem'}}>Rs.{item.priceOld}</span>}
-                                {item.offer && <span className="discount" style={{fontSize: '1.25rem'}}>{item.offerOFF}</span>}
-                            </div>
-                        </div>
-                        <div className="card-footer">
-                            <button className="button btn-primary card-button">ADD TO CART</button>
-                        </div>
-                    </div>
-                </div>
-                      )
-                })}
-
-        
-               
-
-                
+                })}   
             </div>
         
         </section>
