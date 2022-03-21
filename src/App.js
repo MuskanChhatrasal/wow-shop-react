2e1dfb49e0adc726289f394e7dad1a5eeeaeac5e
Muskan <muskanchhatrasal@gmail.com>
2022-03-21 16:45:08 +0530

    Setup: Routing for login and signup

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Footer from '../src/Components/Footer/footer'
 import Cart from '../src/Components/Cart/cart'
 import Wishlist from '../src/Components/Wishlist/wishlist'
 import Mockman from "mockman-js";
+import Login from "./Components/Authentication/login";
+import Signup from "./Components/Authentication/signup";
 
 
 function App() {
@@ -19,6 +21,8 @@ function App() {
       <Route path="/cart" element={<Cart />} />
       <Route path="/wishlist" element={<Wishlist />}/>
       <Route path="/mockman" element={<Mockman />} />
+      <Route path="/login" element={<Login />}/>
+      <Route path="/signup" element={<Signup />}/>
     </Routes>
     <Footer />
     </>
