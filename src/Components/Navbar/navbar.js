e41c0d67f32542ef3eff30b494da9c3486a04c7b
Muskan <muskanchhatrasal@gmail.com>
2022-03-24 07:57:55 +0530

    Style: logout style added

--- src/Components/Navbar/navbar.js
+++ src/Components/Navbar/navbar.js
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from 'react'
 import './navbar.css'
 import { Link } from 'react-router-dom'
 import { useFilter } from '../../Context/context'
+import { useAuth } from '../../Context/AuthContext/authContext'
 
 const Navbar = () => {
   
   const {cartState} = useFilter();
   const [cartQuantity, setCartQuantity] = useState(cartState.cart)
+  const {authState: {userName, loggedIn}, logout } = useAuth();
 
   useEffect(()=>{
     setCartQuantity(cartState.cart.length)
@@ -31,12 +33,23 @@ const Navbar = () => {
 
       <div className="icons">
         <div className="fas fa-bars" id="menu-btn"></div>
+        <Link to='/login'><i class="fas fa-user-circle profile-icon" style={{fontSize: '3rem'}}></i></Link>
+        <span className='userName'>{userName}</span>
         <Link to='/cart'><div className="fas fa-shopping-cart" id="cart-btn" style={{position: 'relative'}}>
         <span style={{position: 'absolute', top: -20, right: 0, fontSize: '1.5rem'}}>{cartQuantity}</span>
         </div></Link>
-        <Link to='/login'><button className='loginButton'>Login In</button></Link>
         {/* <div className="fas fa-user" id="login-btn">
         </div> */}
+        {/* <li className={`${loggedIn ? "loggedIn" : "loggedNotDispaly" }`}>
+                    <Link className="login" to="/login">
+                       <i class="fas fa-sign-out-alt logout-icon" onClick={()=> logout()}></i>
+                    </Link>
+                    <span className="logout-txt">LogOut</span>
+                </li> */}
+
+              <Link to='/login'><i className="fas fa-sign-out-alt" id= {loggedIn ? "logout-btn":'logoutNotDisplay'}  style={{position: 'relative'}}>
+              <span style={{position: 'absolute', marginTop: '4rem', marginLeft: '-4rem', fontSize: '1.25rem'}} className='logout-txt'>logOut</span>
+              </i></Link>
       </div>
     </header>
     </div>
